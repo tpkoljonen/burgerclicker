161cb42da506cdce6044e72196b772f8c9e699fb
Timo-Pekka Koljonen <holjis36@live.com>
2020-02-22 17:04:39 +0200

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,88 +1,27 @@
-import React, {Component, useState} from 'react';
+import React, {Component} from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+
+import Menu from './Menu';
+import Game from './Game';
+import Coupons from './Coupons';
+import Profile from './Profile';
 
 import './App.css';
 
-import burger from './burger.png';
-import iconburger from './icon-burger.png';
-import iconcoupon from './icon-coupon.png';
-import iconboy from './icon-boy.png';
-
 class Clicker extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      clicks: 0
-    }
-    this.mouseClicked = this.mouseClicked.bind(this);
-  }
-
-  mouseClicked() {
-    const clicks = this.state.clicks;
-    this.setState({
-      clicks: clicks +1
-    })
-  }
 
   render() {
     return (
-      <div className="clicker">
-        <div className="header">
-          <h1>Burger Clicker</h1>
+      <Router>
+        <div className="clicker">
+          <Route path="/" exact component={Game} />
+          <Route path="/coupons" component={Coupons} />
+          <Route path="/profile" component={Profile} />
+          <Menu claimableCoupons={5}/>
         </div>
-        <div className="content content--justified">
-        <Stats count={this.state.clicks} />
-        <Burger onClick={this.mouseClicked}/>
-        <Booster boost={3.2}/>
-        </div>
-        <Menu claimableCoupons={5}/>
-      </div>
+      </Router>
     );
   }
-}
-
-function Stats(props) {
-  return(
-    <div className="stats">
-      <div className="stats_title">Burgers</div>
-      <div className="stats_count">{props.count}</div>
-    </div>
-  );
-}
-
-function Burger(props) {
-  const [pressed, setPressed] = useState(false);
-  const classValue = pressed ? "burger__img burger__img--pressed" : "burger__img";
-  return (
-    <div className="burger">
-      <img 
-        src={burger} 
-        alt="" 
-        className={classValue}
-        onClick={props.onClick}
-        onPointerDown={() => setPressed(true)}
-        onPointerUp={() => setPressed(false)}
-      />
-    </div>
-  );
-}
-
-function Booster(props) {
-  return (
-    <div className="booster">
-      {props.boost} burgers / click
-    </div>
-  )
-}
-
-function Menu(props) {
-  return(
-    <div className="menu">
-      <div><img src={iconburger} alt="game" /></div>
-      <div><img src={iconcoupon} alt="coupons" /><span className="badge">{props.claimableCoupons}</span></div>
-      <div><img src={iconboy} alt="profile" /></div>
-    </div>
-  )
 }
 
 export default Clicker;
---
+++ src/Booster.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Booster(props) {
+    return (
+      <div className="booster">
+        {props.boost} burgers / click
+      </div>
+    )
+}
+
+export default Booster;
